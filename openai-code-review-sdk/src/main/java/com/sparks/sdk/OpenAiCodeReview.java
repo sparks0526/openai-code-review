398974f10daaf989d7a610e671d0db2efaae1d8e
m0_52219209 <m0_52219209@noreply.gitcode.com>
2025-06-24 16:24:32 +0800

    feat: 检出代码，输出diff

--- openai-code-review-sdk/src/main/java/com/sparks/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/sparks/sdk/OpenAiCodeReview.java
@@ -1,8 +1,36 @@
 package com.sparks.sdk;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("测试执行");
+
+        // 1. 代码检出
+
+        // 创建并设置外部进程构建器
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        // 启动进程并获取输入流
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        // 读取并存储到StringBuilder中
+        String line;
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        // 等待进程结束并输出退出码
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 }
