38564d82e02a23b1a55885ac124cf8d8e141420b
m0_52219209 <m0_52219209@noreply.gitcode.com>
2025-06-26 14:13:19 +0800

    feat: 写入log日志文件 测试v5.1

--- openai-code-review-test/src/test/java/com/sparks/test/ApiTest.java
+++ openai-code-review-test/src/test/java/com/sparks/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaaa4"));
+        System.out.println(Integer.parseInt("abc"));
 
     }
 }
